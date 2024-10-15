a1e04db64db37b6299d380aefbaeb9b66472ef55
majid nazari <majidnazarister@gmail.com>
2024-10-15 14:53:47 +0330

    add clanmember CRUD

--- app/Models/ClanMember.php
+++ app/Models/ClanMember.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ClanMember extends Model
 {
-    protected $fillable = ['creator_id', 'editor_id', 'Clan_id', 'node_code'];
+    protected $fillable = ['creator_id', 'editor_id', 'clan_id', 'node_code'];
     use HasFactory,SoftDeletes;
 
     public function Creator()
@@ -24,6 +24,6 @@ class ClanMember extends Model
     
     public function Clan()
     {
-        return $this->belongsTo(Clan::class, 'Clan_id');
+        return $this->belongsTo(Clan::class, 'clan_id');
     }
 }
