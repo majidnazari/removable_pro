2c1d550122bdd795c23a47e257849f96e01aa2dd
majid nazari <majidnazarister@gmail.com>
2024-11-17 12:30:35 +0330

    chech the same mbile

--- app/GraphQL/Mutations/UserMergeRequest/sendRequestToOtherFamily.php
+++ app/GraphQL/Mutations/UserMergeRequest/sendRequestToOtherFamily.php
@@ -31,23 +31,23 @@ final class sendRequestToOtherFamily
     }
     public function resolveUserMergeRequest($rootValue, array $args, GraphQLContext $context = null, ResolveInfo $resolveInfo)
     {
-        
-         $user_sender_id=auth()->guard('api')->user()->id;
 
-        if ( $user_sender_id === $args['user_sender_id']) {
+        $user_sender_id = auth()->guard('api')->user()->id;
+
+        if ($user_sender_id === $args['user_sender_id']) {
             return Error::createLocatedError("the sender and reciver cannot be the same!");
         }
 
-        $person=Person::where('id',$args['node_sender_id'])->first();
+        $person = Person::where('id', $args['node_sender_id'])->first();
 
-        if( $person && empty($person->country_code)  && empty($person->mobile)){
+        if ($person && empty($person->country_code) && empty($person->mobile)) {
             return Error::createLocatedError("the person with this mobile not found please try again.");
         }
-       
+
         $user_reciver = User::where('mobile', $person->country_code . $person->mobile)
             //->where('is_owner',true)
             //->where(column: 'country_code',operator: $args['country_code'])
-            ->where('id', '!=',  $user_sender_id)
+            ->where('id', '!=', $user_sender_id)
             //->where('is_owner', true)
             ->where('status', Status::Active)
             ->first();
@@ -58,13 +58,16 @@ final class sendRequestToOtherFamily
         if (!$user_reciver) {
             return Error::createLocatedError("the node you have seleted not found!");
         }
- 
-      
+
+        if (!$user_reciver->id === $user_sender_id) {
+            return Error::createLocatedError("the sender and reciver cannot be the same!");
+        }
+
 
         //Log::info("the args are:" . json_encode($args));
         //$user_id=auth()->guard('api')->user()->id;
         $UserMergeRequestResult = [
-            "user_sender_id" =>  $user_sender_id,
+            "user_sender_id" => $user_sender_id,
             "node_sender_id" => $args['node_sender_id'],
             "user_reciver_id" => $user_reciver->id,
             // "node_reciver_id" => $args['node_reciver_id'],
@@ -75,11 +78,11 @@ final class sendRequestToOtherFamily
         ];
 
         Log::info("the args are:" . json_encode($UserMergeRequestResult));
-        $is_exist = UserMergeRequest::where('user_sender_id',  $user_sender_id)
-        ->where('node_sender_id', $args['node_sender_id'])
-        ->where('user_reciver_id', $user_reciver->id)
-        ->where('request_status', RequestStatus::Susspend)
-        ->first();
+        $is_exist = UserMergeRequest::where('user_sender_id', $user_sender_id)
+            ->where('node_sender_id', $args['node_sender_id'])
+            ->where('user_reciver_id', $user_reciver->id)
+            ->where('request_status', RequestStatus::Susspend)
+            ->first();
         if ($is_exist) {
             return Error::createLocatedError("UserMergeRequest-CREATE-RECORD_IS_EXIST");
         }
