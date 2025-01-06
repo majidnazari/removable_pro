00900105870a0ac0e2f391b1d7587274d8021afa
majid nazari <majidnazarister@gmail.com>
2025-01-06 13:47:28 +0330

    add GC to talent header

--- database/seeders/MajorFieldSeeder.php
+++ database/seeders/MajorFieldSeeder.php
@@ -2,8 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\major;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
+
 
 class MajorFieldSeeder extends Seeder
 {
@@ -12,6 +16,20 @@ class MajorFieldSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $json = File::get(database_path('datasample/majorfield.json')); 
+        $majors = json_decode($json, true); // Decode JSON into an array
+      
+        $majorData = [];
+        foreach ($majors as $major) {
+           
+            $title = !empty($major['title']) ? $major['title'] : null;
+            $majorData[] = [
+                'title' => $title, 
+               
+            ];
+        }
+       
+        DB::table('major_fields')->insert($majorData);
+
     }
 }
