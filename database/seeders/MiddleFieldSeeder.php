eccc5ef764a9b3a6906f5930677f0efafc3ac977
majid nazari <majidnazarister@gmail.com>
2025-01-06 13:58:33 +0330

    ass major filed seedd

--- database/seeders/MiddleFieldSeeder.php
+++ database/seeders/MiddleFieldSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\major;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
 
 class MiddleFieldSeeder extends Seeder
 {
@@ -12,6 +15,19 @@ class MiddleFieldSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $json = File::get(database_path('datasample/middlefield.json')); 
+        $middles = json_decode($json, true); // Decode JSON into an array
+      
+        $middleData = [];
+        foreach ($middles as $middle) {
+           
+            $title = !empty($middle['title']) ? $middle['title'] : null;
+            $middleData[] = [
+                'title' => $title, 
+                'major_field_id' => $middle['major_field_id']
+            ];
+        }
+       
+        DB::table('middle_fields')->insert($middleData);
     }
 }
--- database/seeders/MinorFieldSeeder.php
+++ database/seeders/MinorFieldSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\major;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
 
 class MinorFieldSeeder extends Seeder
 {
@@ -12,6 +15,20 @@ class MinorFieldSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $json = File::get(database_path('datasample/minorfield.json')); 
+        $minors = json_decode($json, true); // Decode JSON into an array
+      
+        $minorData = [];
+        foreach ($minors as $minor) {
+           
+            $title = !empty($minor['title']) ? $minor['title'] : null;
+            $minorData[] = [
+                'title' => $title, 
+                'middle_field_id' => $minors['middle_field_id']
+               
+            ];
+        }
+       
+        DB::table('minor_fields')->insert($minorData);
     }
 }
