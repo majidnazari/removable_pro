b7c8ad98694902cfb0007f321fa29ec3551405ae
majid nazari <majidnazarister@gmail.com>
2025-01-07 12:38:25 +0330

    change the validation each person of talent header into checkout in MR

--- app/Rules/Share/CheckPersonOfEachUser.php
+++ app/Rules/Share/CheckPersonOfEachUser.php
@@ -6,6 +6,8 @@ use Illuminate\Contracts\Validation\Rule;
 use App\Traits\AuthUserTrait;
 use App\Traits\FindOwnerTrait;
 use App\Models\Person;
+use Illuminate\Support\Facades\DB;
+use App\GraphQL\Enums\MergeStatus;
 
 use Log;
 
@@ -46,11 +48,30 @@ class CheckPersonOfEachUser implements Rule
         // Log::info("The person id is: " . $personId);
         // Log::info("the person id is:" .$this->personId);
         // Check if the person has any active children
-        $person = Person::where('id', $this->personId)->where('creator_id', $this->getUserId())->exists();
+        $allowedCreatorIds = $this->getAllowedCreatorIds($this->getUserId());
+        $person = Person::where('id', $this->personId)->whereIn('creator_id', $allowedCreatorIds)->exists();
 
-       // Log::info("the person is :" . json_encode($person));
+        //Log::info(message: "the allowedCreatorIds is :" . json_encode($allowedCreatorIds));
         return $person;
 
+    }
+    protected function getAllowedCreatorIds(int $userId): array
+    {
+        // Start with the logged-in user ID
+        $allowedCreatorIds = [$userId];
+
+        // Get all user_receiver_id values where the logged-in user is the sender and status is Complete (4)
+        $connectedUserIds = DB::table('user_merge_requests')
+            ->where('user_sender_id', $userId)
+            ->where('status', MergeStatus::Complete->value) // Status = Complete
+            ->whereNull('deleted_at') // Exclude soft-deleted records
+            ->pluck('user_receiver_id')
+            ->toArray();
+
+        // Merge the connected user IDs into the allowed IDs
+        $allowedCreatorIds = array_merge($allowedCreatorIds, $connectedUserIds);
+       // Log::info("the all alowed user are:" . json_encode($allowedCreatorIds));
+        return $allowedCreatorIds;
     }
 
     public function message()
