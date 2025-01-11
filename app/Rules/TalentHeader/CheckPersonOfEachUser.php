c9ff968585a287cab14b875b23905bdc0d122cf4
majid nazari <majidnazarister@gmail.com>
2025-01-11 11:05:51 +0330

    .

--- app/Rules/TalentHeader/CheckPersonOfEachUser.php
+++ app/Rules/TalentHeader/CheckPersonOfEachUser.php
@@ -29,30 +29,8 @@ class CheckPersonOfEachUser implements Rule
     public function passes($attribute, $value)
     {
         $this->personId = $value;
-        // Log the entire request payload for debugging
-
-        //$requestData = request()->all();
-        //Log::info("the person id is :" . json_encode($value));
-
-        // Attempt to extract `person_id` directly from the `variables` or fallback to parsing the query
-        // $personId = null;
-
-        // // Check if `variables` exist and contain `input.person_id`
-        // if (!empty($requestData['variables']['input']['person_id'])) {
-        //     $personId = $requestData['variables']['input']['person_id'];
-        // } else {
-        //     // Parse `query` to extract `person_id` if it's inline
-        //     if (!empty($requestData['query'])) {
-        //         preg_match('/person_id\s*:\s*"(\d+)"/', $requestData['query'], $matches);
-        //         $personId = $matches[1] ?? null;
-        //     }
-        // }
-        // $this->personId = $personId;
-        //Log::info("Extracted person_id: " . $personId);
-
-        // Log::info("The person id is: " . $personId);
+       
         // Log::info("the person id is:" .$this->personId);
-        // Check if the person has any active children
         $allowedCreatorIds = $this->getAllowedUserIds($this->getUserId());
 
         //Log::info("the all user alowed are :" . json_encode($allowedCreatorIds));
@@ -68,11 +46,6 @@ class CheckPersonOfEachUser implements Rule
             $this->errorMessage = "this person is owner and you cannot set talent to him/her !";
             return false;
         }
-
-        // log::info("the person is :" . json_encode($person));
-        // if (($person) && ($person->is_owner == true) && ($person->creator_id != $this->getUserId())) {
-        //     return false;
-        // }
         //Log::info(message: "the allowedCreatorIds is :" . json_encode($allowedCreatorIds));
         return $person;
 
