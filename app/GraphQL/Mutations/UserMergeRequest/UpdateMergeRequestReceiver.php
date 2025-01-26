a69faea07c1e319d3e1b9013eff98f992bc2cde3
majid nazari <majidnazarister@gmail.com>
2025-01-26 13:06:24 +0330

    add condition complete to check merge requerst to validation exist one active record

--- app/GraphQL/Mutations/UserMergeRequest/UpdateMergeRequestReceiver.php
+++ app/GraphQL/Mutations/UserMergeRequest/UpdateMergeRequestReceiver.php
@@ -7,7 +7,7 @@ use App\Models\UserMergeRequest;
 use GraphQL\Type\Definition\ResolveInfo;
 use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
 use GraphQL\Error\Error;
-use App\GraphQL\Enums\Status;
+use App\GraphQL\Enums\MergeStatus;
 use App\GraphQL\Enums\RequestStatusSender;
 use App\models\User;
 use App\models\Person;
@@ -68,21 +68,23 @@ final class UpdateMergeRequestReceiver
             // ->where('user_receiver_id', $user_receiver->id)
             ->where('request_status_sender', RequestStatusSender::Active->value)
             ->where('request_status_receiver', RequestStatusReceiver::Active->value)
-            ->where('merge_status_sender', RequestStatusSender::Active->value);
-        //->first();
+            ->where('merge_status_sender', RequestStatusSender::Active->value)
+            ->where('status', '!=', MergeStatus::Complete->value)
+
+        ->first();
 
 
         // Log the SQL query with parameters
-        $sql = $is_exist->toSql();
-        $bindings = $is_exist->getBindings();
+        // $sql = $is_exist->toSql();
+        // $bindings = $is_exist->getBindings();
 
-        Log::info('SQL Query for Merge Request:', [
-            'query' => $sql,
-            'bindings' => $bindings,
-        ]);
+        // Log::info('SQL Query for Merge Request:', [
+        //     'query' => $sql,
+        //     'bindings' => $bindings,
+        // ]);
 
-        // Execute the query
-        $is_exist = $is_exist->first();
+        // // Execute the query
+        // $is_exist = $is_exist->first();
 
         if ($is_exist) {
             return Error::createLocatedError("UserMergeRequest-YOU_HAVE_ONE_ACTIVE_REQUEST");
